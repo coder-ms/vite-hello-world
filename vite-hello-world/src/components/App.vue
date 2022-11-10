19f24b14eb970635a0cbccbf94df65f775c5f7e3
coder-ms <marco.sbrascia@gmail.com>
2022-11-10 09:21:02 +0200

    hello world

---
+++ vite-hello-world/src/components/App.vue
@@ -0,0 +1,25 @@
+
+<template>
+    <h1> {{ message }} </h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hello world !'
+        }
+    },
+    methods: {
+
+    },
+}
+</script>
+
+
+
+<style lang="css">
+h1 {
+    color: red;
+}
+</style>
